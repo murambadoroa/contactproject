c8ea892d41c59b278b3315b7c201d30cbe0cd045
Gavin Murambadoro <developer.gavin@gmail.com>
2022-03-01 14:32:08 +0200

    Added mvc

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class DefaultController extends AbstractController
 {
@@ -13,6 +17,39 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return new Response("Hello, world!");
+        return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/contact-us", name="app_contact_us")
+     */
+    public function contact(Request $request, EntityManagerInterface $manager, ValidatorInterface $validator): Response
+    {
+        if ($request->request->getBoolean('submit')) {
+            $name = $request->request->get('name');
+            $email = $request->request->get('email');
+
+            $contact = new Contact();
+            $contact->setName($name);
+            $contact->setEmail($email);
+
+            // todo: validate
+            $errors = $validator->validate($contact);
+
+            if (count($errors)) {
+                dd((string)$errors);
+            }
+
+            // todo: save to db
+            $manager->persist($contact);
+            $manager->flush();
+
+            // todo: send email
+
+
+            return $this->redirectToRoute("app_default");
+        }
+
+        return $this->render('contact.html.twig');
     }
 }
---
+++ src/Entity/Contact.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\Column(type="string", length=100, name="name")
+     * @Assert\Length(min=3, max=100)
+     */
+    private ?string $name = "";
+
+    /**
+     * @ORM\Column(type="string", length=100, name="email")
+     * @Assert\Length(min=3, max=100)
+     */
+    private ?string $email = "";
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
